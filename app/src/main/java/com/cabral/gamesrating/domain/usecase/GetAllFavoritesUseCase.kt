2d9a0d55c7480513a5a40ba24b925291f61fb79b
“William8Cabral” <suporte@vxm.com.br>
2026-04-09 08:34:12 -0300

    usecase test

--- app/src/main/java/com/cabral/gamesrating/domain/usecase/GetAllFavoritesUseCase.kt
+++ app/src/main/java/com/cabral/gamesrating/domain/usecase/GetAllFavoritesUseCase.kt
@@ -1,6 +1,5 @@
 package com.cabral.gamesrating.domain.usecase
 
-import com.cabral.gamesrating.data.local.GameFavoriteEntity
 import com.cabral.gamesrating.domain.repository.GamesRepository
 import com.cabral.gamesrating.ui.model.GameUi
 import kotlinx.coroutines.flow.Flow
