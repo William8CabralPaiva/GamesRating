ab6aff5bee05db39085965ba2a0beb78443d76d1
“William8Cabral” <suporte@vxm.com.br>
2026-04-10 19:53:33 -0300

    instrumental test

--- app/src/main/java/com/cabral/gamesrating/ui/components/GameItem.kt
+++ app/src/main/java/com/cabral/gamesrating/ui/components/GameItem.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
@@ -94,13 +95,12 @@ fun GameItem(
                                         isFavoriteState = !isFavoriteState
                                         it.isFavorite = isFavoriteState
                                         onClickFavorite(gameUi)
-                                        it.isFavorite = isFavoriteState
-                                        onClickFavorite(gameUi)
                                     }
                                 },
                                 modifier = Modifier
                                     .padding(0.dp)
                                     .align(Alignment.TopEnd)
+                                    .testTag("favorite_button")
                             ) {
                                 Icon(
                                     imageVector = if (isFavoriteState) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
@@ -162,6 +162,5 @@ fun GameItemPreview() {
         genres = "Ação, Aventura",
         false
     )
-    GameItem(gameUi, false)
     GameItem(gameUi, false)
 }
