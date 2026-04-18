be34f0f7af7843c592cbea25dd7f8e0362e79f0a
“William8Cabral” <suporte@vxm.com.br>
2026-04-18 10:44:37 -0300

    splash

--- app/src/main/java/com/cabral/gamesrating/MainActivity.kt
+++ app/src/main/java/com/cabral/gamesrating/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.core.os.LocaleListCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.cabral.gamesrating.navigation.BottomNavigationBar
@@ -31,6 +32,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var settingsManager: SettingsManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
